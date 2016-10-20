26420cbcb9f41b408841bf0b098a38f386b23168
hongjiaohu <h.hu@sjasd.ca>
2016-10-20 09:58:44 -0500

    A2,P1
    Change to hello-user,and add some new content.

---
+++ frmHellouser.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Hello_user
+{
+    partial class frmHellouser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblqusetion = new System.Windows.Forms.Label();
+            this.lblDispaly = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txtOutput1 = new System.Windows.Forms.TextBox();
+            this.lblInput1 = new System.Windows.Forms.Label();
+            this.txtOutput2 = new System.Windows.Forms.TextBox();
+            this.lblinput2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblqusetion
+            // 
+            this.lblqusetion.AutoSize = true;
+            this.lblqusetion.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblqusetion.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lblqusetion.Location = new System.Drawing.Point(186, 27);
+            this.lblqusetion.Name = "lblqusetion";
+            this.lblqusetion.Size = new System.Drawing.Size(340, 33);
+            this.lblqusetion.TabIndex = 0;
+            this.lblqusetion.Text = "How much money do you have?";
+            // 
+            // lblDispaly
+            // 
+            this.lblDispaly.Location = new System.Drawing.Point(218, 163);
+            this.lblDispaly.Name = "lblDispaly";
+            this.lblDispaly.Size = new System.Drawing.Size(277, 133);
+            this.lblDispaly.TabIndex = 1;
+            this.lblDispaly.Click += new System.EventHandler(this.lblDispaly_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(280, 299);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(141, 53);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Click it";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // txtOutput1
+            // 
+            this.txtOutput1.Location = new System.Drawing.Point(395, 77);
+            this.txtOutput1.Name = "txtOutput1";
+            this.txtOutput1.Size = new System.Drawing.Size(100, 20);
+            this.txtOutput1.TabIndex = 3;
+            this.txtOutput1.TextChanged += new System.EventHandler(this.txtOutput1_TextChanged);
+            // 
+            // lblInput1
+            // 
+            this.lblInput1.AutoSize = true;
+            this.lblInput1.Location = new System.Drawing.Point(218, 77);
+            this.lblInput1.Name = "lblInput1";
+            this.lblInput1.Size = new System.Drawing.Size(78, 13);
+            this.lblInput1.TabIndex = 4;
+            this.lblInput1.Text = "Monthly wages";
+            // 
+            // txtOutput2
+            // 
+            this.txtOutput2.Location = new System.Drawing.Point(395, 123);
+            this.txtOutput2.Name = "txtOutput2";
+            this.txtOutput2.Size = new System.Drawing.Size(100, 20);
+            this.txtOutput2.TabIndex = 5;
+            // 
+            // lblinput2
+            // 
+            this.lblinput2.AutoSize = true;
+            this.lblinput2.Location = new System.Drawing.Point(218, 123);
+            this.lblinput2.Name = "lblinput2";
+            this.lblinput2.Size = new System.Drawing.Size(93, 13);
+            this.lblinput2.TabIndex = 6;
+            this.lblinput2.Text = "Year-end bonuses";
+            // 
+            // frmHellouser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 364);
+            this.Controls.Add(this.lblinput2);
+            this.Controls.Add(this.txtOutput2);
+            this.Controls.Add(this.lblInput1);
+            this.Controls.Add(this.txtOutput1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblDispaly);
+            this.Controls.Add(this.lblqusetion);
+            this.Name = "frmHellouser";
+            this.Text = "Hello user program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblqusetion;
+        private System.Windows.Forms.Label lblDispaly;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txtOutput1;
+        private System.Windows.Forms.Label lblInput1;
+        private System.Windows.Forms.TextBox txtOutput2;
+        private System.Windows.Forms.Label lblinput2;
+    }
+}
+
---
+++ frmHellouser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_user
+{
+    public partial class frmHellouser : Form
+    {
+        public frmHellouser()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            /**
+
+ *
+
+ * Name: Hongjiao Hu
+
+ * Teacher: Mr. Hardman
+ 
+ * Assignment #2, Program #1
+
+ * Date Last Modified: 10/14/2016
+
+ *
+
+ */
+
+
+            //user input
+            double input1 = Convert.ToDouble(txtOutput1.Text);
+            double input2 = Convert.ToDouble(txtOutput2.Text);
+
+            double total = input1 * 12 + input2;
+
+            //computer display
+            lblDispaly.Text = string.Format("{0,10}{1,15:C2}\n{2,10}{3,15:C2}\n{4,10}{5,15:C2}",
+                "Monthly wages", input1, "Year-end bonuses", input2, "Total:", total);
+        }
+
+        private void lblDispaly_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOutput1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
